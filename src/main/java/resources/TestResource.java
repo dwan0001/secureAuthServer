c615dd495974a3fd78c47290c23b65d098adf475
kari helene <kari.hdb@gmail.com>
2016-07-25 18:56:04 +0200

    fix backend. Works now

--- src/main/java/resources/TestResource.java
+++ src/main/java/resources/TestResource.java
@@ -4,14 +4,15 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/test")
-public class TestResource {
+public class TestResource{
 	
 	@GET
 	@Path("/hello")
 	@Produces(MediaType.APPLICATION_JSON)
-	public void testOutput(){
-		
+	public String testOutput(){
+		return "hei";	
 	}
 }
